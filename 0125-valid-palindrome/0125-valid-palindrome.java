a2697d29388e97efe15d6634a1e1b0a718c790bc
niharika-25bisen <24bisenn@rbunagpur.in>
2025-08-12 20:22:04 +0530

    Time: 2 ms (98.7%), Space: 43 MB (76.51%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,32 @@
+public class Solution {
+    public boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+
+            
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        String input = "A man, a plan, a canal: Panama";
+        System.out.println("Is palindrome? " + sol.isPalindrome(input));
+    }
+}
